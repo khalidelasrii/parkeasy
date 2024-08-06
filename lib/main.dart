bc906bd2ead1008af435a5eba998ca835546168a
khalidelasrii <khalidelasri534@gmail.com>
2024-08-06 15:14:20 +0100

    add  injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:parkeasy/firebase_options.dart';
+import 'package:parkeasy/service_locator.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await di.init(); // Initialiser les services avec GetIt
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
---
+++ lib/service_locator.dart
@@ -0,0 +1,38 @@
+import 'package:get_it/get_it.dart';
+import 'package:parkeasy/features/auth/data/datasources/firebase_auth_service/firebase_auth_service.dart';
+import 'package:parkeasy/features/auth/data/datasources/firebase_firestor_service/firebase_firestor_service.dart';
+import 'package:parkeasy/features/auth/data/datasources/firebase_services.dart';
+import 'package:parkeasy/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:parkeasy/features/auth/domain/repositories/auth_repository.dart';
+import 'package:parkeasy/features/auth/domain/usecases/sign_in_with_phone_use_case.dart';
+import 'package:parkeasy/features/auth/domain/usecases/verification_o_t_p_use_case.dart';
+import 'package:parkeasy/features/auth/presentation/bloc/auth_bloc/auth_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // Register Firebase services
+  sl.registerLazySingleton<FirebaseAuthService>(() => FirebaseAuthService());
+  sl.registerLazySingleton<FirebaseFirestorService>(
+      () => FirebaseFirestorService());
+
+  // Register Firebase services implementation
+  sl.registerLazySingleton<FirebaseServices>(
+    () => FirebaseServicesImpl(
+      firebaseAuthService: sl<FirebaseAuthService>(),
+      firebaseFirestorService: sl<FirebaseFirestorService>(),
+    ),
+  );
+
+  // Register repositories
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(firebaseServices: sl<FirebaseServices>()),
+  );
+
+  // Register use cases
+  sl.registerLazySingleton(() => SignInWithPhoneUseCase(sl<AuthRepository>()));
+  sl.registerLazySingleton(() => VerificationOTPUseCase(sl<AuthRepository>()));
+
+  // Register BLoC
+  sl.registerFactory(() => AuthBloc(repository: sl<AuthRepository>()));
+}
